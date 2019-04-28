81aefe4f1f5083f5875750bcd536fe1a5a39c3cb
Hayden Elza <hayden.elza@gmail.com>
2019-04-28 09:49:15 -0500

    Added tile-search page

---
+++ tile-search/assets/metadata.js
@@ -0,0 +1,53 @@
+var metadata = {
+	"cityOfMadison": {
+		"name": "Cify of Madison",
+		"deliveryName": "Madison 2016 City Delivery",
+		"deliveryYear": 2016,
+		"lasURLs": {
+			null: [
+				"ftp://ftp.ssec.wisc.edu/pub/wisconsinview/lidar/Dane/Madison_2016_City_Delivery/First_%20Return_LAS/",
+				".las"
+			]
+		},
+		"demURLs": {
+			null: [
+				"ftp://ftp.ssec.wisc.edu/pub/wisconsinview/lidar/Dane/Madison_2016_City_Delivery/Raster_DEM_Tiles/",
+				".flt",
+				".flt.aux.xml",
+				".flt.ovr",
+				".hdr",
+				".prj"
+			]
+		},
+		"terrainURLs": {},
+		"breaklines": "ftp://ftp.ssec.wisc.edu/pub/wisconsinview/lidar/Dane/Madison_2016_City_Delivery/Hydro_Breaklines/",
+		"contours": "N/A",
+		"deliveryFolder": "ftp://ftp.ssec.wisc.edu/pub/wisconsinview/lidar/Dane/Madison_2016_City_Delivery/"
+	},
+	"daneCounty": {
+		"name": "Dane County",
+		"deliveryName": "Dane 2017 3DEP Delivery",
+		"deliveryYear": 2017,
+		"lasURLs": {
+			"esri": [
+				"ftp://ftp.ssec.wisc.edu/pub/wisconsinview/lidar/Dane/Dane_2017_3DEP_Delivery/Classified_LAS/ESRI/",
+				"_esri.las"
+			],
+			"usgs": [
+				"ftp://ftp.ssec.wisc.edu/pub/wisconsinview/lidar/Dane/Dane_2017_3DEP_Delivery/Classified_LAS/USGS/",
+				"_usgs.las"
+			]
+		},
+		"demURLs": {
+			null: [
+				"ftp://ftp.ssec.wisc.edu/pub/wisconsinview/lidar/Dane/Dane_2017_3DEP_Delivery/Raster_DEM_Tiles/",
+				".img"
+			]
+		},
+		"terrainURLs": {},
+		"breaklines": "ftp://ftp.ssec.wisc.edu/pub/wisconsinview/lidar/Dane/Dane_2017_3DEP_Delivery/Hydro_Breaklines/",
+		"contours": "N/A",
+		"deliveryFolder": "ftp://ftp.ssec.wisc.edu/pub/wisconsinview/lidar/Dane/Dane_2017_3DEP_Delivery/"
+	}
+
+};
