355920d2b196fb2a008d03a9ef6d413912a12f15
Hayden Elza <hayden.elza@gmail.com>
2019-05-03 08:45:38 -0500

    Added iron geojson and metadata, moved map script to main, changed hightlighting to blue, added persistent selection

---
+++ tile-search/assets/main.js
@@ -0,0 +1,179 @@
+// Highlight and dehighlight logic
+function highlight(layer) {
+	layer.setStyle({
+		weight: 8,
+		color: '#15d8db',
+		dashArray: '',
+		fillOpacity: 0.4
+	});
+
+	if (!L.Browser.ie && !L.Browser.opera) {
+		layer.bringToFront();
+		selected.bringToFront();
+	}
+}
+
+function dehighlight(layer) {
+	if (selected === null || selected._leaflet_id !== layer._leaflet_id) {
+		indexLayer.resetStyle(layer);
+	}
+}
+
+function select (layer) {
+	if (selected !== null) {
+		var previous = selected;
+	}
+	// map.fitBounds(layer.getBounds());
+	selected = layer;
+	if (previous) {
+		dehighlight(previous);
+	}
+}
+
+var selected = null;
+
+// END highlight and dehighlight logic
+
+
+function featureClick(e) {
+	// Update sidebar
+
+	// Get metadata
+	var tileName = e.sourceTarget.feature.properties.tileName
+	var deliveryName = metadata[layerName].deliveryName
+	var lasURLs = metadata[layerName].lasURLs;
+	var demURLs = metadata[layerName].demURLs;
+	var dsmURLs = metadata[layerName].dsmURLs;
+	var breaklines = metadata[layerName].breaklines;
+	var contours = metadata[layerName].contours;
+	var delivery = metadata[layerName].deliveryFolder;
+
+	// Build html content string
+	var html = 
+		// '<h1>' + 
+		// 	metadata[e.sourceTarget.feature.properties.sourceName].name +
+		// '</h1>' +
+		'<h2>' +
+			deliveryName +
+		'</h2><hr>' +
+		'<h3>Tile ' + tileName + '</h3>' +
+		'<b>LAS:</b><br><div class="download-links">'+ genLinks(tileName,lasURLs) + '</div>' +
+		'<br>' +
+		'<b>DEM:</b><div class="download-links">' + genLinks(tileName,demURLs) + '</div>' +
+		'<br>' +
+		'<b>DSM:</b><div class="download-links">' + genLinks(tileName,dsmURLs) + '</div>' +
+		'<br>' +
+		'<b>Breaklines:</b> ' + genLinks(tileName,breaklines) +
+		'<br><br>' +
+		'<b>Contours:</b><div class="download-links"> ' + genLinks(tileName,contours) + '</div>' +
+		'<br>' +
+		'<b>Delivery Folder:</b> ' + genLinks(tileName,delivery) + '<br><br>';			;
+
+	// Insert html string into document
+	$("#sidebar").html(html);
+
+	layer.bringToFront();
+
+}
+
+function genLinks(tileName,urls) {
+	// If string not object
+	if (typeof urls === 'string') {
+		var url = urls;
+		if (urls == "N/A") { return url }
+		else { return '<a target="_blank" href="' + url + '">directory</a>' }
+	}
+
+	// Prepare html blob
+	var htmlContent = "";
+	
+	for (source in urls) {
+		
+		var url = urls[source];
+
+		// If only one source
+		if (source == "null") { 
+			var links = "";
+			for (var i = 1; i < url.length; i++) {
+				links = links + '<a target="_blank" href="' + url[0] + tileName + url[i] + '">' + tileName + url[i] + '</a><br>'
+			}
+			links = links +  '<a target="_blank" href="' + url[0] + '">directory</a>'
+			return links
+		}
+
+		// If multiple sources
+		if (htmlContent != "") {htmlContent = htmlContent + "<br>"}
+		htmlContent = htmlContent + '<a target="_blank" href="' + url[0] + tileName + url[1] + '">' + source + '</a> <a target="_blank" href="' + url[0] + '">directory</a>';
+	}
+	return htmlContent
+}
+
+function onEachFeature(feature, layer) {
+
+	layer.on({
+		'mouseover': function (e) {
+			highlight(e.target);
+		},
+		'mouseout': function (e) {
+			dehighlight(e.target);
+		},
+		'click': function (e) {
+			select(e.target);
+			featureClick(e);
+		}
+	});
+
+}
+
+
+
+// Set initial view
+var map = L.map('map');
+
+// Get acquisition 
+var urlParams = new  URLSearchParams(window.location.search);
+var layerName = urlParams.get('layer');
+
+// Add Base Layer
+L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
+	attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+	maxZoom: 18,
+	id: 'mapbox.streets',
+	accessToken: 'pk.eyJ1IjoiZWx6YSIsImEiOiJjanVlNWZ5N2cwb2pyNDR0YzV2YmtteTB4In0.6BAcP2DqZG34BAyy7ug75Q'
+}).addTo(map);
+
+// Add WI Counties Layer
+var countyStyle = {
+	"fillOpacity": 0,
+	"color": "#000",
+	"weight": 3,
+	"opacity": 0.2
+};
+var countiesLayer = L.geoJSON(wiCounties, {style: countyStyle});
+countiesLayer.addTo(map);
+map.fitBounds(countiesLayer.getBounds());
+
+// Tile Index
+var indexStyle = {
+	"fillOpacity": 0,
+	"color": "#000",
+	"weight": 1,
+	"opacity": 1
+};
+
+
+
+
+
+// Add tile index
+var indexLayer = L.geoJson.ajax("layers/" + layerName + ".geojson", {
+	style: indexStyle,
+	onEachFeature: onEachFeature
+})
+indexLayer.addTo(map);
+
+// Zoom to 
+indexLayer.addTo(this.map);
+indexLayer.on('data:loaded', function() {
+	this.map.fitBounds(indexLayer.getBounds());
+}.bind(this));
--- tile-search/assets/metadata.js
+++ tile-search/assets/metadata.js
@@ -5,7 +5,7 @@ var metadata = {
 		"deliveryYear": 2016,
 		"lasURLs": {
 			null: [
-				"ftp://ftp.ssec.wisc.edu/pub/wisconsinview/lidar/Dane/Madison_2016_City_Delivery/First_%20Return_LAS/",
+				"ftp://ftp.ssec.wisc.edu/pub/wisconsinview/lidar/Dane/Madison_2016_City_Delivery/Classified_LAS/LAS/",
 				".las"
 			]
 		},
@@ -19,7 +19,12 @@ var metadata = {
 				".prj"
 			]
 		},
-		"terrainURLs": {},
+		"dsmURLs": {
+			null: [
+				"ftp://ftp.ssec.wisc.edu/pub/wisconsinview/lidar/Dane/Madison_2016_City_Delivery/First_%20Return_LAS/",
+				".las"
+			]
+		},
 		"breaklines": "ftp://ftp.ssec.wisc.edu/pub/wisconsinview/lidar/Dane/Madison_2016_City_Delivery/Hydro_Breaklines/",
 		"contours": "N/A",
 		"deliveryFolder": "ftp://ftp.ssec.wisc.edu/pub/wisconsinview/lidar/Dane/Madison_2016_City_Delivery/"
@@ -44,10 +49,53 @@ var metadata = {
 				".img"
 			]
 		},
-		"terrainURLs": {},
+		"dsmURLs": "N/A",
 		"breaklines": "ftp://ftp.ssec.wisc.edu/pub/wisconsinview/lidar/Dane/Dane_2017_3DEP_Delivery/Hydro_Breaklines/",
 		"contours": "N/A",
 		"deliveryFolder": "ftp://ftp.ssec.wisc.edu/pub/wisconsinview/lidar/Dane/Dane_2017_3DEP_Delivery/"
+	},
+	"iron": {
+		"name": "Iron County",
+		"deliveryName": "Iron 2015 County Delivery",
+		"deliveryYear": 2015,
+		"lasURLs": {
+			null: [
+				"ftp://ftp.ssec.wisc.edu/pub/wisconsinview/lidar/Iron/Iron_2015_County_Delivery/Raster_DSM_Tiles/IronCo",
+				".las"
+			]
+		},
+		"demURLs": {
+			null: [
+				"ftp://ftp.ssec.wisc.edu/pub/wisconsinview/lidar/Iron/Iron_2015_County_Delivery/Raster_DEM_Tiles/IronCo",
+				"_elev.flt",
+				"_elev.hdr",
+				"_elev.prj"
+			]
+		},
+		"dsmURLs": {
+			null: [
+				"ftp://ftp.ssec.wisc.edu/pub/wisconsinview/lidar/Iron/Iron_2015_County_Delivery/Raster_DSM_Tiles/IronCo",
+				"_elev.flt",
+				"_elev.flt.aux.xml",
+				"_elev.flt.ovr",
+				"_elev.hdr",
+				"_elev.prj"
+			]
+		},
+		"breaklines": "ftp://ftp.ssec.wisc.edu/pub/wisconsinview/lidar/Iron/Iron_2015_County_Delivery/Hydro_Breaklines/",
+		"contours": {
+			null: [
+				"ftp://ftp.ssec.wisc.edu/pub/wisconsinview/lidar/Iron/Iron_2015_County_Delivery/Contours_2ft/SHP/IronCo",
+				".cpg",
+				".dbf",
+				".prj",
+				".sbn",
+				".sbx",
+				".shp",
+				".shx"
+			]
+		},
+		"deliveryFolder": "ftp://ftp.ssec.wisc.edu/pub/wisconsinview/lidar/Iron/Iron_2015_County_Delivery/"
 	}
 
 };
