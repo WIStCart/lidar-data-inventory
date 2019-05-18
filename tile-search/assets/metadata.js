e058079bf2d2f63e623f67bd49e4b7f9c80fad76
Hayden Elza <HaydenElza@users.noreply.github.com>
2019-05-17 19:42:33 -0500

    Added Adams, Ashland, Buffalo, Burnett, Calumet, and Crawford counties

--- tile-search/assets/metadata.js
+++ tile-search/assets/metadata.js
@@ -96,6 +96,159 @@ var metadata = {
 			]
 		},
 		"deliveryFolder": "ftp://ftp.ssec.wisc.edu/pub/wisconsinview/lidar/Iron/Iron_2015_County_Delivery/"
+	},
+	"adams": {
+		"name": "Adams County",
+		"deliveryName": "Adams 2010 County Delivery",
+		"deliveryYear": 2010,
+		"lasURLs": {
+			null: [
+				"ftp://ftp.ssec.wisc.edu/pub/wisconsinview/lidar/Adams/Adams_2010_County_Delivery/Classified_LAS/",
+				".las"
+			]
+		},
+		"demURLs": "ftp://ftp.ssec.wisc.edu/pub/wisconsinview/lidar/Adams/Adams_2010_County_Delivery/Raster_DEM_County/DEM/",
+		"dsmURLs": "ftp://ftp.ssec.wisc.edu/pub/wisconsinview/lidar/Adams/Adams_2010_County_Delivery/Derived_Terrain/",
+		"breaklines": "ftp://ftp.ssec.wisc.edu/pub/wisconsinview/lidar/Adams/Adams_2010_County_Delivery/Hydro_Breaklines/SHP/",
+		"contours": {
+			null: [
+				"ftp://ftp.ssec.wisc.edu/pub/wisconsinview/lidar/Adams/Adams_2010_County_Delivery/Contours_2ft/SHP/",
+				".dbf",
+				".prj",
+				".shp",
+				".shp.xml",
+				".shx"
+			]
+		},
+		"deliveryFolder": "ftp://ftp.ssec.wisc.edu/pub/wisconsinview/lidar/Adams/Adams_2010_County_Delivery/"
+	},
+	"ashland": {
+		"name": "Ashland County",
+		"deliveryName": "Ashland 2014 FEMA Delivery",
+		"deliveryYear": 2014,
+		"lasURLs": {
+			null: [
+				"ftp://ftp.ssec.wisc.edu/pub/wisconsinview/lidar/Ashland/Ashland_2014_FEMA_Delivery/Classified_LAS/",
+				".las"
+			]
+		},
+		"demURLs": "N/A",
+		"dsmURLs": "N/A",
+		"breaklines": "ftp://ftp.ssec.wisc.edu/pub/wisconsinview/lidar/Ashland/Ashland_2014_FEMA_Delivery/Hydro_Breaklines/",
+		"contours": "N/A",
+		"deliveryFolder": "ftp://ftp.ssec.wisc.edu/pub/wisconsinview/lidar/Ashland/Ashland_2014_FEMA_Delivery/"
+	},
+	"buffalo": {
+		"name": "Buffalo County",
+		"deliveryName": "Buffalo 2016 3DEP Delivery",
+		"deliveryYear": 2016,
+		"lasURLs": {
+			null: [
+				"ftp://ftp.ssec.wisc.edu/pub/wisconsinview/lidar/Buffalo/Buffalo_2016_3DEP_Delivery/Classified_LAS/",
+				".las"
+			]
+		},
+		"demURLs": {
+			null: [
+				"ftp://ftp.ssec.wisc.edu/pub/wisconsinview/lidar/Buffalo/Buffalo_2016_3DEP_Delivery/Raster_DEM_Tiles/",
+				".img"
+			]
+		},
+		"dsmURLs": "N/A",
+		"breaklines": "ftp://ftp.ssec.wisc.edu/pub/wisconsinview/lidar/Buffalo/Buffalo_2016_3DEP_Delivery/Hydro_Breaklines/",
+		"contours": "ftp://ftp.ssec.wisc.edu/pub/wisconsinview/lidar/Buffalo/Buffalo_2016_3DEP_Delivery/Contours/",
+		"deliveryFolder": "ftp://ftp.ssec.wisc.edu/pub/wisconsinview/lidar/Buffalo/Buffalo_2016_3DEP_Delivery/"
+	},
+	"burnett": {
+		"name": "Burnett County",
+		"deliveryName": "Burnett 2015 County Delivery",
+		"deliveryYear": 2015,
+		"lasURLs": {
+			null: [
+				"ftp://ftp.ssec.wisc.edu/pub/wisconsinview/lidar/Burnett/Burnett_2015_County_Delivery/Classifield_LAS/LAS/",
+				".las"
+			]
+		},
+		"demURLs": {
+			null: [
+				"ftp://ftp.ssec.wisc.edu/pub/wisconsinview/lidar/Burnett/Burnett_2015_County_Delivery/Rasters_DEM_Tiles/",
+				"_elev.flt",
+				"_elev.hdr",
+				"_elev.prj"
+			]
+		},
+		"dsmURLs": {
+			null: [
+				"ftp://ftp.ssec.wisc.edu/pub/wisconsinview/lidar/Burnett/Burnett_2015_County_Delivery/Rasters_DSM_Tiles/",
+				"_elev.flt",
+				"_elev.hdr",
+				"_elev.prj"
+			]
+		},
+		"breaklines": "ftp://ftp.ssec.wisc.edu/pub/wisconsinview/lidar/Burnett/Burnett_2015_County_Delivery/Hydro_Breaklines/",
+		"contours": {
+			null: [
+				"ftp://ftp.ssec.wisc.edu/pub/wisconsinview/lidar/Burnett/Burnett_2015_County_Delivery/Contours_2ft/SHP/",
+				".cpg",
+				".dbf",
+				".prj",
+				".shp",
+				".shx"
+			]
+		},
+		"deliveryFolder": "ftp://ftp.ssec.wisc.edu/pub/wisconsinview/lidar/Burnett/Burnett_2015_County_Delivery/"
+	},
+	"calumet": {
+		"name": "Calumet County",
+		"deliveryName": "Calumet 2005 County Delivery",
+		"deliveryYear": 2005,
+		"lasURLs": {
+			null: [
+				"ftp://ftp.ssec.wisc.edu/pub/wisconsinview/lidar/Calumet/Calumet_2005_County_Delivery/Bare_Earth_Point_Cloud/BE_LAS/Tile",
+				".las",
+				".lasx"
+			]
+		},
+		"demURLs": {
+			null: [
+				"ftp://ftp.ssec.wisc.edu/pub/wisconsinview/lidar/Calumet/Calumet_2005_County_Delivery/Raster_DEM_Tiles_16ft/ARC/tile",
+				"/",
+				".aux"
+			]
+		},
+		"dsmURLs": {
+			null: [
+				"ftp://ftp.ssec.wisc.edu/pub/wisconsinview/lidar/Calumet/Calumet_2005_County_Delivery/Raster_DSM_Tiles_16ft/ARC/tile",
+				"/",
+				".aux"
+			]
+		},
+		"breaklines": "ftp://ftp.ssec.wisc.edu/pub/wisconsinview/lidar/Calumet/Calumet_2005_County_Delivery/Hydro_Breaklines/",
+		"contours": "ftp://ftp.ssec.wisc.edu/pub/wisconsinview/lidar/Calumet/Calumet_2005_County_Delivery/Contours_2ft/",
+		"deliveryFolder": "ftp://ftp.ssec.wisc.edu/pub/wisconsinview/lidar/Calumet/Calumet_2005_County_Delivery/"
+	},
+	"crawford": {
+		"name": "Crawford County",
+		"deliveryName": "Crawford 2011 County Delivery",
+		"deliveryYear": 2011,
+		"lasURLs": {
+			"LAS": [
+				"ftp://ftp.ssec.wisc.edu/pub/wisconsinview/lidar/Crawford/Crawford_2011_County_Delivery/Classified_LAS/LAS/",
+				""
+			],
+			"SHP": [
+				"ftp://ftp.ssec.wisc.edu/pub/wisconsinview/lidar/Crawford/Crawford_2011_County_Delivery/Classified_LAS/SHP/",
+				""
+			]
+		},
+		"demURLs": "ftp://ftp.ssec.wisc.edu/pub/wisconsinview/lidar/Crawford/Crawford_2011_County_Delivery/Raster_DEM_County/",
+		"dsmURLs": "N/A",
+		"breaklines": "ftp://ftp.ssec.wisc.edu/pub/wisconsinview/lidar/Crawford/Crawford_2011_County_Delivery/Hydro_Breaklines/",
+		"contours": {
+			null: [
+				"ftp://ftp.ssec.wisc.edu/pub/wisconsinview/lidar/Crawford/Crawford_2011_County_Delivery/Contours_2ft/"
+			]
+		},
+		"deliveryFolder": "ftp://ftp.ssec.wisc.edu/pub/wisconsinview/lidar/Crawford/Crawford_2011_County_Delivery/"
 	}
-
 };
